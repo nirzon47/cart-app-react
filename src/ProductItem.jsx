e674ca6672bcc4ab28adad23b160436e3ff43acd
Nirzon Karmakar <nirzon47@gmail.com>
2023-12-24 16:33:57 +0530

    made the products list into a json and made a loop that renders all the data

--- src/ProductItem.jsx
+++ src/ProductItem.jsx
@@ -1,6 +1,23 @@
 import { MdKeyboardArrowDown, MdKeyboardArrowUp } from 'react-icons/md'
 
+import { useState } from 'react'
+
 const ProductItem = ({ name, src, price }) => {
+	const [quantity, setQuantity] = useState(1)
+
+	const increaseQuantity = () => {
+		setQuantity(quantity + 1)
+	}
+
+	const decreaseQuantity = () => {
+		if (quantity > 1) {
+			setQuantity(quantity - 1)
+		} else {
+		}
+	}
+
+	const removeItem = () => {}
+
 	return (
 		<div className='flex justify-between w-[30rem]'>
 			<div className='flex gap-8'>
@@ -14,9 +31,13 @@ const ProductItem = ({ name, src, price }) => {
 				</div>
 			</div>
 			<div className='flex flex-col items-center justify-center gap-1'>
-				<MdKeyboardArrowUp className='text-2xl text-blue-600 cursor-pointer hover:text-blue-800' />
-				<span className='text-xl font-medium text-zinc-800'>1</span>
-				<MdKeyboardArrowDown className='text-2xl text-blue-600 cursor-pointer hover:text-blue-800' />
+				<button onClick={increaseQuantity}>
+					<MdKeyboardArrowUp className='text-2xl text-blue-600 cursor-pointer hover:text-blue-800' />
+				</button>
+				<span className='text-xl font-medium text-zinc-800'>{quantity}</span>
+				<button onClick={decreaseQuantity}>
+					<MdKeyboardArrowDown className='text-2xl text-blue-600 cursor-pointer hover:text-blue-800' />
+				</button>
 			</div>
 		</div>
 	)
